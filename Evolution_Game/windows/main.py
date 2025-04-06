bea903065a630c14e06efaf1d1ba2d537da87106
TimePrecursor <21006@fraser.school.nz>
2025-04-06 16:55:49 +1200

    V 1.7 - Finished implementing the buttons and button effects

--- Evolution_Game/windows/main.py
+++ Evolution_Game/windows/main.py
@@ -1,4 +1,4 @@
-codeVersion= "1.06"
+codeVersion= "1.7"
 
 import arcade
 from arcade.gui import *
@@ -31,11 +31,11 @@ class MyView(UIView):
         self.background_color = arcade.color.AMAZON
 
         grid = UIGridLayout(
-            column_count=10,
+            column_count=3,
             row_count=5,
             size_hint=(0, 0),
             vertical_spacing=0,
-            horizontal_spacing=0,
+            horizontal_spacing=10,
         )
 
         self.ui.add(UIAnchorLayout(children=[grid]))
@@ -52,82 +52,44 @@ class MyView(UIView):
         # self.box = arcade.gui.UIGridLayout()
 
         '''
-        0 = Normal Texture
-        1 = Pressed Texture
-        2 = Hover Texture
+        CarnivoreIcons = 0
+        HerbivoreIcons = 1
+        DecomposerIcons = 2
         '''
-        sprite_list = {
-            "CarnivoreIcons": ["CarnivoreFinal2.png", "CarnivoreFinal2Pressed.png", "CarnivoreFinal2Hover.png"],
-            "HerbivoreIcons": ["HerbivoreFinal2.png", "HerbivoreFinal2Hover.png", "HerbivoreFinal2Pressed.png"],
-            "DecomposerIcons": ["DecomposerFinal2.png" , "DecomposerFinal2Hover.png", "DecomposerFinal2Pressed.png"],
 
+        sprite_list = {
+            0: ["CarnivoreFinal2.png", "CarnivoreFinal2Hover.png", "CarnivoreFinal2Pressed.png"],
+            1: ["HerbivoreFinal2.png", "HerbivoreFinal2Hover.png", "HerbivoreFinal2Pressed.png"],
+            2: ["DecomposerFinal2.png" , "DecomposerFinal2Hover.png", "DecomposerFinal2Pressed.png"],
         }
 
-        # sprite_list2 = {
-        #     "CarnivoreFinal2.png" : [0,"carnivore_sprite_Normal"],
-        #     "HerbivoreFinal2.png" : [0,"herbivore_sprite_Normal"],
-        #     "DecomposerFinal2.png" : [0,"decomposer_sprite_Normal"],
-        #     "CarnivoreFinal2Hover.png": [1, "carnivore_sprite_Hover"],
-        #     "HerbivoreFinal2Hover.png": [1, "herbivore_sprite_Hover"],
-        #     "DecomposerFinal2Hover.png": [1, "decomposer_sprite_Hover"],
-        #     "CarnivoreFinal2Pressed.png": [2, "carnivore_sprite_Pressed"],
-        #     "HerbivoreFinal2Pressed.png": [2, "herbivore_sprite_Pressed"],
-        #     "DecomposerFinal2Pressed.png": [2, "decomposer_sprite_Pressed"]
-        # }
+        # If I have sprites or buttons, I will create them here:
 
-
-        # If I have sprites, I will create them here
-        # This may help:
-        # ac.sprite_creation(self,name.png",1,1)
-        # The offset percent default is 1 AKA (100%)
         self.sprites = arcade.SpriteList()
-        index1 = 0
 
-        for count, i in enumerate(sprite_list.keys()):
-            print(count)
-            index1 += 1
-            value = sprite_list[count] if i[0] == index1 else None
-            # for x in range(0,3):
-
-            # Define the filename to search for:
-            file_path = os.path.join('..', 'assets', value)  # This goes up one level from 'windows' folder
-
-            # Loading textures to a specific file path:
-            sprite_Norm_TEX = arcade.load_texture(file_path)
-            sprite_Pressed_TEX = arcade.load_texture(file_path)
-            sprite_Hover_TEX = arcade.load_texture(file_path)
-
-            # Create the Texture Button:
+        for count, i in enumerate(sprite_list):
             texture_button = UITextureButton(
                 text="",
                 width=300,
-                height=300,
-                texture=sprite_Norm_TEX,
-                texture_hovered=sprite_Hover_TEX,
-                texture_pressed=sprite_Pressed_TEX,
+                height=300
             )
-            if sprite_list[i][0] == 0:
-                grid.add(texture_button, row=0, column=int(0+index1))
+            file_path_list = []
+            for x in range(0,3):
+                # Define the filename to search for:
+                file_path = os.path.join('..', 'assets', sprite_list[count][x])
+                file_path_list.append(file_path)
 
-            # index1 += 0.5
-            # sprite_texture = arcade.load_texture(file_path)
-            # xsprite = arcade.Sprite(sprite_texture)
-            # xsprite.draw_hit_box()
-            # xsprite.sync_hit_box_to_texture()
-            # xsprite.center_x = ((WINDOW_WIDTH / 2) * index1)
-            # xsprite.center_y = ((WINDOW_HEIGHT / 2) + 0)
-            # self.sprites.append(xsprite)
+            # Loading textures to a specific file path:
+            sprite_Norm_TEX = arcade.load_texture(file_path_list[0])
+            sprite_Hover_TEX = arcade.load_texture(file_path_list[1])
+            sprite_Pressed_TEX = arcade.load_texture(file_path_list[2])
 
-        # self.carnivore_sprite = ac.sprite_creation(self, "assets/Carnivores1.png", 100,1)
-        # self.herbivore_sprite = ac.sprite_creation(self, "assets/Herbivores1.png",1,1)
-        # self.decomposer_sprite = ac.sprite_creation(self, "assets/Decomposers1.png",1,1)
+            # Set the loaded texture to each button
+            texture_button.texture = sprite_Norm_TEX
+            texture_button.texture_pressed = sprite_Pressed_TEX
+            texture_button.texture_hovered = sprite_Hover_TEX
 
-    # def sprite_creation(self, sprite_file, offset_X, offset_Y):
-    #     self.sprite_texture = arcade.load_texture(sprite_file)
-    #     self.first_sprite = arcade.Sprite(self.sprite_texture)
-    #     self.first_sprite.center_x = ((WINDOW_WIDTH / 2) + offset_X)
-    #     self.first_sprite.center_y = ((WINDOW_HEIGHT / 2) + offset_Y)
-    #     return self.first_sprite
+            grid.add(texture_button, row=1, column=int(0 + count))
 
     def reset(self):
         """Reset the game to the initial state."""
@@ -193,7 +155,7 @@ class MyView(UIView):
 def main():
     """ Main function """
     # Create a window class.
-    window = arcade.Window(800, 600, WINDOW_TITLE, resizable=True)
+    window = arcade.Window(1000, 600, WINDOW_TITLE, resizable=True)
 
 
     # Show GameView on screen
