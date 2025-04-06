23386765b9f6e16703681df00b0e2ba6348edff1
TimePrecursor <21006@fraser.school.nz>
2025-04-06 17:08:59 +1200

    V 1.71 - Deleted 'on_draw()' from the code. (I can add it later IF needed) - Moved all code related to creating the icon buttons to 'setup_buttons()' - changed base window width to 1000

--- Evolution_Game/windows/main.py
+++ Evolution_Game/windows/main.py
@@ -1,4 +1,4 @@
-codeVersion= "1.7"
+codeVersion= "1.71"
 
 import arcade
 from arcade.gui import *
@@ -30,53 +30,38 @@ class MyView(UIView):
         super().__init__()
         self.background_color = arcade.color.AMAZON
 
-        grid = UIGridLayout(
+        self.grid = UIGridLayout(
             column_count=3,
             row_count=5,
             size_hint=(0, 0),
             vertical_spacing=0,
             horizontal_spacing=10,
         )
+        self.ui.add(UIAnchorLayout(children=[self.grid]))
 
-        self.ui.add(UIAnchorLayout(children=[grid]))
 
-        # setting up window variables:
+        # If I have sprites or buttons, I will create them here:
 
-        # self.grid = UIGridLayout(
-        #     column_count=3,
-        #     row_count=4,
-        #     size_hint=(0, 0),
-        #     vertical_spacing=10,
-        #     horizontal_spacing=10,
-        # )
-        # self.box = arcade.gui.UIGridLayout()
-
-        '''
-        CarnivoreIcons = 0
-        HerbivoreIcons = 1
-        DecomposerIcons = 2
-        '''
-
-        sprite_list = {
+        self.sprite_list = {
             0: ["CarnivoreFinal2.png", "CarnivoreFinal2Hover.png", "CarnivoreFinal2Pressed.png"],
             1: ["HerbivoreFinal2.png", "HerbivoreFinal2Hover.png", "HerbivoreFinal2Pressed.png"],
             2: ["DecomposerFinal2.png" , "DecomposerFinal2Hover.png", "DecomposerFinal2Pressed.png"],
         }
-
-        # If I have sprites or buttons, I will create them here:
-
         self.sprites = arcade.SpriteList()
+        self.setup_buttons()
 
-        for count, i in enumerate(sprite_list):
-            texture_button = UITextureButton(
-                text="",
-                width=300,
-                height=300
-            )
+    def reset(self):
+        """Reset the game to the initial state."""
+        # Do changes needed to restart the game here if you want to support that
+        pass
+
+    def setup_buttons(self):
+        for count, i in enumerate(self.sprite_list):
+            texture_button = UITextureButton(text="", width=300, height=300)
             file_path_list = []
             for x in range(0,3):
                 # Define the filename to search for:
-                file_path = os.path.join('..', 'assets', sprite_list[count][x])
+                file_path = os.path.join('..', 'assets', self.sprite_list[count][x])
                 file_path_list.append(file_path)
 
             # Loading textures to a specific file path:
@@ -89,29 +74,7 @@ class MyView(UIView):
             texture_button.texture_pressed = sprite_Pressed_TEX
             texture_button.texture_hovered = sprite_Hover_TEX
 
-            grid.add(texture_button, row=1, column=int(0 + count))
-
-    def reset(self):
-        """Reset the game to the initial state."""
-        # Do changes needed to restart the game here if you want to support that
-        pass
-
-
-    # def on_draw(self):
-    #     """
-    #     Render the screen.
-    #     """
-    #
-    #     # This command should happen before we start drawing. It will clear
-    #     # the screen to the background color, and erase what we drew last frame.
-    #     self.clear()
-    #     #stuff here:
-    #     self.sprites.draw()
-    #     # arcade.draw_sprite(self.carnivore_sprite)
-    #     # arcade.draw_sprite(self.herbivore_sprite)
-    #     # arcade.draw_sprite(self.decomposer_sprite)
-
-
+            self.grid.add(texture_button, row=1, column=int(0 + count))
 
     def on_update(self, delta_time):
         """
