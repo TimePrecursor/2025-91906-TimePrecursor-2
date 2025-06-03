3231cce034fb227b01406efd31a99a90d6347282
21006 <21006@fraser.schoo.nz>
2025-06-04 10:52:20 +1200

    V 1.40.3.0 - School LOLLZERS

--- Evolution_Game/depricated_files/testingprint.py
+++ Evolution_Game/depricated_files/testingprint.py
@@ -1,2 +1,45 @@
-list = ["1","+","3"]
-print("what is",10**2,"2")
+import random
+import math
+import numpy
+
+difficulty = input("Choose your difficulty: ")
+num_float = float(difficulty)  # Convert string to float
+num_rounded = round(num_float)  # Round the float number
+difficulty = int(num_rounded)    # Convert the rounded number to int
+
+print(f"Chosen difficulty is {difficulty}")
+
+
+def sqrt1(n, value):
+    return value ** (1 / n)
+
+def power1(n,value):
+    superscripts = {
+        '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴',
+        '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹',
+        '+': '⁺', '-': '⁻', '=': '⁼', '(': '⁽', ')': '⁾',
+        'n': 'ⁿ', 'i': 'ⁱ'
+    }
+    return [(value ** n),superscripts[str(n)]]
+
+# Name: [function, sign]
+lvl_signs = {
+    "1": {'add': ["+","+"], 'subtract': ["-","-"]},
+    "2": {'times': ["*", "×"], 'divide': ["/", "÷"]},
+    "3": {'sqrt1': ["sqrt1","√"], 'power': ["^","power1"]}
+    }
+
+def getsign(x):
+    y = lvl_signs[x]
+    print(y)
+
+getsign(difficulty)
+print(power1(2,10))
+
+def question1():
+    questionlist = [
+        random.randint(1,10),
+        random.choice()
+        random.gauss
+    ]
+    print("What is ",x)
